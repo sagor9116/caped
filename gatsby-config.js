0884b2e00f5d704a649cc9cea79d9103d3fae56e
sagor9116 <sagor.roy9116@gmail.com>
2022-03-03 18:59:15 +0600

    setup basic developing environment
    setup gatsby for front-end, setup shopfiy store for products, setup test files for testing and other related files that should enhance the develpoing experience in any projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,48 @@
-module.exports = {
-    siteMetadata: {
-      title: `new`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
+require("dotenv").config()
 
-    ]
+module.exports = {
+  siteMetadata: {
+    siteTitle: "CAPED",
+    siteTitleDefault: "Caped Clothing",
+    siteUrl: "https://shopify-demo.gatsbyjs.com",
+    hrefLang: "en",
+    siteDescription: "A Gatsby powered E-commmerce clothing site for woman",
+  },
+  flags: {
+    FAST_DEV: true,
+    FAST_REFRESH: true,
+    DETECT_NODE_MUTATIONS: true,
+    PARALLEL_SOURCING: true,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-gatsby-cloud",
+
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        shopName: "caped",
+        password: process.env.GATSBY_SHOPIFY_SHOP_PASSWORD,
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+      },
+    },
+  ].filter(Boolean),
 }
