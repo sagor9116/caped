cef2e7cffcd6b74861d9a9e0a223251f01526740
sagor9116 <sagor.roy9116@gmail.com>
2022-03-19 01:29:47 +0600

    Added navigation, favicon and test files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Navigation, Announcement } from "../../components"
+import Title from "../title/Title"
 
 const Header = ({ menuLinks, siteTitle }) => {
   return (
     <div>
       <Announcement />
       <Navigation menuLinks={menuLinks} siteTitle={siteTitle} />
+      <Title />
     </div>
   )
 }
